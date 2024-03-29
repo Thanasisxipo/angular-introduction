0928e587754183a3f8b8d80a0aaaf32a59d528a3
Thanasis Xypolytos <thanasis.xipo@gmail.com>
2024-03-29 20:59:54 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { WellcomeComponent } from './components/wellcome/wellcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'event-bind-example', component: EventBindExampleComponent},
+  { path: 'wellcome', component: WellcomeComponent},
+  { path: '', redirectTo: '/wellcome', pathMatch: 'full'}
+];
