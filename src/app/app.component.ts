347d3c2a8230710cb813921df726176fd6b1510d
Thanasis Xypolytos <thanasis.xipo@gmail.com>
2024-03-29 19:13:54 +0200

    component-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,19 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 export class AppComponent {
   name = 'Thanassis';
 
+  person0: Person  = {
+      givenName: 'Thanassis',
+      surName: 'Xypolytos',
+      age: 35,
+      email: 'thanasis.xipo@gamil.com',
+      address: 'Athens, Greece'
+    };
+
+    person1: Person  = {
+      givenName: 'John',
+      surName: 'Snow',
+      age: 36,
+      email: 'john@gamil.com',
+      address: 'New york, USA'
+    };
 }
